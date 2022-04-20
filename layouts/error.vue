8f1d97ad8a06f68eebead40c7bbdc98061f345d9
benjamincloquet <benjamin.cloquet@gmail.com>
2022-04-20 15:09:20 +0200

    Add error message

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="flex flex-col gap-4 items-center font-sans pt-8">
     <p class="text-8xl">{{ error.statusCode || 'Error' }}</p>
+    <p>{{ error.message }}</p>
     <h1>Oops ! An error occurred.</h1>
     <NuxtLink class="underline" to="/">Take me back home</NuxtLink>
   </main>
